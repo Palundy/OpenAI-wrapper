8da4d72dd77926616f6269a8ec6c8c1b6f687488
Plont <palauvanhelden02@gmail.com>
2023-11-13 12:00:20 +0100

    Conversations are now called threads, and added functionality for the AI to visit pages.

--- ChatCompletions.php
+++ ChatCompletions.php
@@ -7,7 +7,7 @@ class ChatCompletions {
 
 
     // Instance variables
-    private $Model = "gpt-4";
+    private $Model = "gpt-4-1106-preview";
     private $SystemMessage = "You are a helpful assistant.";
     private $MaxTokens = 256;
     private $MessageThread = [];
@@ -20,6 +20,7 @@ class ChatCompletions {
     private $storeObject = false;
     private $ChatCompletionId = null;
     private $hasImages = false;
+    private $initializedFunctions = null;
 
 
 
@@ -30,20 +31,22 @@ class ChatCompletions {
         $this->URL = OpenAI::_API_URL . OpenAI::_CHAT_COMPLETIONS_ENDPOINT;
 
         // Check whether to retrieve a stored conversation.
-        if ($ChatCompletionId === false) {
-            return;
+        if ($ChatCompletionId !== false) {
+            // Retrieve the stored conversation
+            $serializedObject = file_get_contents(
+                __DIR__ . "/objects/" . $ChatCompletionId . ".txt"
+            );
+            
+            // Deserialize the object
+            $deserializedObject = unserialize($serializedObject);
+            foreach($deserializedObject as $Key => $Value) {
+                $this->$Key = $Value;
+            }
         }
-        
-        // Retrieve the stored conversation
-        $serializedObject = file_get_contents(
-            __DIR__ . "/objects/" . $ChatCompletionId . ".txt"
-        );
-        
-        // Deserialize the object
-        $deserializedObject = unserialize($serializedObject);
-        foreach($deserializedObject as $Key => $Value) {
-            $this->$Key = $Value;
-        }
+
+        // Retrieve the functions stored in the `functions.json` file
+        $this->initializedFunctions = file_get_contents(__DIR__ ."/functions.json");
+        $this->initializedFunctions = json_decode($this->initializedFunctions, true);
     }
     
     /**
@@ -52,7 +55,18 @@ class ChatCompletions {
      * @return void
      */
     public function __destruct() {
+        // Store the thread
+        $this->storeObject();
+    }
 
+
+    /**
+     * Stores a hardcopy of the thread.
+     * Only if `$this->storeObject === true`.
+     * 
+     * @return void
+     */
+    private function storeObject() {
         // Check whether the conversation needs to be stored
         if ($this->storeObject === false) {
             return;
@@ -100,15 +114,17 @@ class ChatCompletions {
 
 
     /**
-     * Sets whether to store the conversation
-     * to be able to later retrieve the same conversation.
+     * Sets whether to store the thread
+     * to be able to later retrieve the same thread.
+     * The thread doesn't get stored on default.
      * 
      * @param bool $Choice
-     * 
+     * Boolean determining if the thread
+     * should be stored. 
      * 
      * @return ChatCompletions
      */
-    public function storeConversation($Choice) {
+    public function storeThread($Choice) {
         $this->storeObject = boolval($Choice);
         return $this;
     }
@@ -207,11 +223,11 @@ class ChatCompletions {
 
 
     /**
-     * Show a crude format of the conversation.
+     * Shows a crude format of the thread.
      * 
      * @return ChatCompletions
      */
-    public function showConversation() {
+    public function showThread() {
         print_r($this->MessageThread);
         return $this;
     }
@@ -236,6 +252,11 @@ class ChatCompletions {
      */
     public function getResponse() {
         $lastMessage = $this->MessageThread[array_key_last($this->MessageThread)];
+        
+        // Check whether the last message was a function call
+        if (isset($lastMessage["tool_calls"])) {
+            return "Function called: ".$lastMessage["tool_calls"][0]["function"]["name"];
+        }
         return $lastMessage["content"];
     }
 
@@ -290,17 +311,22 @@ class ChatCompletions {
 
 
     /**
-     * Starts the conversation.
+     * Starts the thread.
      * 
      * @return ChatCompletions
      */
-    public function startConversation() {
+    public function startThread() {
+
+        // Check whether a thread has already been made
+        if (isset($this->ChatCompletionId)) {
+            return $this->continueThread();
+        }
 
         // Prepend the system message to the beginning of
         // the messages thread
         array_unshift($this->MessageThread, [
             "role" => "system",
-            "content" => $this->SystemMessage    
+            "content" => $this->SystemMessage
         ]);
 
         // Send the request
@@ -309,7 +335,14 @@ class ChatCompletions {
             [
                 "model" => $this->Model,
                 "messages" => $this->MessageThread,
-                "max_tokens" => $this->MaxTokens
+                "max_tokens" => $this->MaxTokens,
+                "tools" => [
+                    [
+                        "type" => "function",
+                        "function" => $this->initializedFunctions[0]
+                    ]
+                ],
+                "tool_choice" => "auto"
             ],
             ["Content-Type: application/json"]
         );
@@ -317,11 +350,117 @@ class ChatCompletions {
         // Check whether the result is `false`
         if (isset($returnData["error"]) === false) {
             $lastChoice = array_pop($returnData["choices"]);
+            $this->finishReason = ($this->hasImages) ? $lastChoice["finish_details"]["type"] : $lastChoice["finish_reason"];
             $this->MessageThread[] = $lastChoice["message"];
             $this->TokenUsage = $returnData["usage"];
-            $this->finishReason = ($this->hasImages) ? $lastChoice["finish_details"]["type"] : $lastChoice["finish_reason"];
             $this->ChatCompletionId = $returnData["id"];
+
+            // Check whether a function has been called
+            if ($this->finishReason == "tool_calls") {
+
+                // Call the chosen function
+                $this->callFunction(
+                    $lastChoice["message"]["tool_calls"][0]["function"]["name"],
+                    json_decode($lastChoice["message"]["tool_calls"][0]["function"]["arguments"], true),
+                    $lastChoice["message"]["tool_calls"][0]["id"]
+                );
+            }
+
+            // Store the thread
+            $this->storeObject();
+        } else {
+            print_r($returnData);
         }
+        return $this;
+    }
+
+
+    /**
+     * Continues the thread.
+     * 
+     * @return ChatCompletions
+     */
+    public function continueThread() {
+
+        // The difference between this function
+        // and ChatCompletions->startThread()
+        // is that this function will the use the existing
+        // thread
+        //      So this function will only work
+        //      if $this->ChatCompletionId is set
+        if (!isset($this->ChatCompletionId)) {
+            return $this->startThread();
+        }
+
+        // Send the request
+        $returnData = cURL::POST(
+            $this->URL,
+            [
+                "model" => $this->Model,
+                "messages" => $this->MessageThread,
+                "max_tokens" => $this->MaxTokens,
+                "tools" => [
+                    [
+                        "type" => "function",
+                        "function" => $this->initializedFunctions[0]
+                    ] 
+                ],
+                "tool_choice" => "auto"
+            ],
+            ["Content-Type: application/json"]
+        );
+
+        // Check whether the result is `false`
+        if (isset($returnData["error"]) === false) {
+            $lastChoice = array_pop($returnData["choices"]);
+            $this->finishReason = ($this->hasImages) ? $lastChoice["finish_details"]["type"] : $lastChoice["finish_reason"];
+            $this->MessageThread[] = $lastChoice["message"];
+            $this->TokenUsage = $returnData["usage"];
+            
+            // The ChatCompletionId doesn't get updated
+            // because if a thread is being continued
+            // it means it already has a ChatCompletionId
+
+            // Check whether a function has been called
+            if ($this->finishReason == "tool_calls") {
+                // Call the chosen function
+                $this->callFunction(
+                    $lastChoice["message"]["tool_calls"][0]["function"]["name"],
+                    json_decode($lastChoice["message"]["tool_calls"][0]["function"]["arguments"], true),
+                    $lastChoice["message"]["tool_calls"][0]["id"]
+                );
+            }
+
+            // Store the thread
+            $this->storeObject();
+        } else {
+            print_r($returnData);
+        }
+        return $this;
+    }
+
+
+    private function callFunction($functionName, $Arguments, $ToolCallId) {
+
+        // cURL::GET() function
+        if ($functionName == "curl_get") {
+            $returnValue = strip_tags(cURL::GET($Arguments["URL"], isset($Arguments["Headers"]) ? explode("\n", $Arguments["Headers"]) : []));
+        }
+
+        // Add the function message
+        $this->addToolsMessage($returnValue, $ToolCallId);
+
+        // Continue the thread
+        $this->continueThread();
+    }
+
+
+    private function addToolsMessage($ToolsMessage, $ToolCallId) {
+        $this->MessageThread[] = [
+            "role" => "tool",
+            "content" => $ToolsMessage,
+            "tool_call_id" => $ToolCallId
+        ];
         return $this;
     }
 }
